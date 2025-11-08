c90e5e740cb24d4f00d839adced5c5ba7d47ee63
Thireus ☠ <Thireus@users.noreply.github.com>
2025-11-08 15:13:57 +0000

    Comment out flash attention argument in index.ts
    Commented out the flash attention argument in the args array.

--- extensions/llamacpp-extension/src/index.ts
+++ extensions/llamacpp-extension/src/index.ts
@@ -1658,7 +1658,7 @@ export default class llamacpp_extension extends AIEngine {
       !cfg.flash_attn ||
       cfg.flash_attn !== ''
     )
-      args.push('--flash-attn', String(cfg.flash_attn)) //default: auto = ON when supported
+    #  args.push('--flash-attn', String(cfg.flash_attn)) //default: auto = ON when supported
 
     // Boolean flags
     if (cfg.ctx_shift) args.push('--context-shift')
